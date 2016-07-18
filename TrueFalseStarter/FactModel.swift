17bf7797188d15e2a1104c62763a53b12320b9f9
serena97 <serena@100.72.219.242>
2016-07-18 15:44:26 +0800

    Refactor the existing code to make use of custom structs or classes where appropriate and ensure that code adheres to the Model-View-Controller (MVC) design pattern. Please place your new custom data structure in a new Swift file.

---
+++ TrueFalseStarter/FactModel.swift
@@ -0,0 +1,19 @@
+//
+//  FactModel.swift
+//  TrueFalseStarter
+//
+//  Created by Serena on 18/7/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+
+struct FactModel{
+    let trivia: [[String : String]] = [
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+}
--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -19,12 +19,7 @@ class ViewController: UIViewController {
     
     var gameSound: SystemSoundID = 0//initial commit test
     
-    let trivia: [[String : String]] = [
-        ["Question": "Only female koalas can whistle", "Answer": "False"],
-        ["Question": "Blue whales are technically whales", "Answer": "True"],
-        ["Question": "Camels are cannibalistic", "Answer": "False"],
-        ["Question": "All ducks are birds", "Answer": "True"]
-    ]
+    let factModel = FactModel()
     
     @IBOutlet weak var questionField: UILabel!
     @IBOutlet weak var trueButton: UIButton!
@@ -46,8 +41,8 @@ class ViewController: UIViewController {
     }
     
     func displayQuestion() {
-        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(trivia.count)
-        let questionDictionary = trivia[indexOfSelectedQuestion]
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(factModel.trivia.count)
+        let questionDictionary = factModel.trivia[indexOfSelectedQuestion]
         questionField.text = questionDictionary["Question"]
         playAgainButton.hidden = true
     }
@@ -68,7 +63,7 @@ class ViewController: UIViewController {
         // Increment the questions asked counter
         questionsAsked += 1
         
-        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
+        let selectedQuestionDict = factModel.trivia[indexOfSelectedQuestion]
         let correctAnswer = selectedQuestionDict["Answer"]
         
         if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
